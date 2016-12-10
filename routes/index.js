0ecc02e58d4f98b07c9eddcca7537f759d160ebe
ilsong <hilsungss@naver.com>
2016-12-11 02:29:12 +0900

    비밀번호찾기

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,10 @@ router.get('/admin', function(req, res) {
   res.render('admin');
 });
 
+router.get('/findpw', function(req, res){
+  res.render('findpw');
+});
+
 /*router.get('/login', function(req, res, next) {
   res.render('login');
 });*/
